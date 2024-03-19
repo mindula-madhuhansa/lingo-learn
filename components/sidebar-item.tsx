9110a226cde71f9fed5a9c7e33051ac140ab5fef
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-19 23:44:36 +0530

    finish sidebar

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+type SidebarItemProps = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export const SidebarItem = ({ label, iconSrc, href }: SidebarItemProps) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <Button
+      variant={active ? "sidebarOutline" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+        <Image
+          src={iconSrc}
+          alt={label}
+          height={32}
+          width={32}
+          className="mr-5"
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};
