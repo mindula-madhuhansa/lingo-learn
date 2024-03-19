c3c91f70512957e518679d975ad128f00c6a057d
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-19 20:57:37 +0530

    create landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lingo Learn",
+  title: "LingoLearn",
   description: "Learn Your Favorite Language",
 };
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={nunito.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={nunito.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
