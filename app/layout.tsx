58c665bb87a8b59213aae11d69f50e5d026df333
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-20 14:00:50 +0530

    add loading, add user progress, users can select courses

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={nunito.className}>{children}</body>
+        <body className={nunito.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
