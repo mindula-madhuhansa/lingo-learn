23fe7326aa1c0ca26c79a9148bcd91fcb13696ca
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-21 01:28:52 +0530

    add lesson page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -5,18 +5,32 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
 import { Unit } from "./unit";
-import { getUnits, getUserProgress } from "@/db/queries";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
   const unitsData = getUnits();
 
-  const [userProgress, units] = await Promise.all([
-    userProgressData,
-    unitsData,
-  ]);
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitsData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
     redirect("/courses");
   }
 
@@ -40,8 +54,8 @@ const LearnPage = async () => {
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
             />
           </div>
         ))}
---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { Header } from "./header";
+
+type QuizProps = {
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  initialHearts: number;
+  initialPercentage: number;
+  userSubscription: any; // TODO: Replace with subscription DB type
+};
+
+export const Quiz = ({
+  initialLessonId,
+  initialLessonChallenges,
+  initialHearts,
+  initialPercentage,
+  userSubscription,
+}: QuizProps) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
