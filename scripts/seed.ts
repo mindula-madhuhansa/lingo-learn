6da451f52e44c68834a644c6b84f3d7527a0f969
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-20 20:59:20 +0530

    write a query to fetch lessons

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -44,6 +49,62 @@ const main = async () => {
         id: 6,
         title: "Russian",
         imageSrc: "/assets/flags/ru.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 3,
+        title: "Unit 01",
+        description: "Learn the basics of Korean",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: "Nouns",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/assets/man.svg",
+        correct: true,
+        text: "남자",
+        audioSrc: "/kr_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/assets/woman.svg",
+        correct: false,
+        text: "여자",
+        audioSrc: "/kr_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/assets/robot.svg",
+        correct: false,
+        text: "로봇",
+        audioSrc: "/kr_robot.mp3",
       },
     ]);
 
