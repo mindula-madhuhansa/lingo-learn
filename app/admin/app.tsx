4465f2b6de9a99fad497946336119630dc3cddce
mindula-madhuhansa <mimmindula@gmail.com@gmail.com>
2024-03-23 18:09:23 +0530

    add react admin dashboard

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -7,6 +7,23 @@ const dataProvider = simpleRestProvider("/api");
 
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
+
+import { UnitList } from "./unit/list";
+import { UnitCreate } from "./unit/create";
+import { UnitEdit } from "./unit/edit";
+
+import { LessonList } from "./lesson/list";
+import { LessonCreate } from "./lesson/create";
+import { LessonEdit } from "./lesson/edit";
+
+import { ChallengeList } from "./challenge/list";
+import { ChallengeCreate } from "./challenge/create";
+import { ChallengeEdit } from "./challenge/edit";
+
+import { ChallengeOptionCreate } from "./challengeOption/create";
+import { ChallengeOptionList } from "./challengeOption/edit";
+import { ChallengeOptionEdit } from "./challengeOption/list";
 
 const App = () => {
   return (
@@ -15,7 +32,41 @@ const App = () => {
         name="courses"
         list={CourseList}
         create={CourseCreate}
+        edit={CourseEdit}
         recordRepresentation="title"
+      />
+
+      <Resource
+        name="units"
+        list={UnitList}
+        create={UnitCreate}
+        edit={UnitEdit}
+        recordRepresentation="title"
+      />
+
+      <Resource
+        name="lessons"
+        list={LessonList}
+        create={LessonCreate}
+        edit={LessonEdit}
+        recordRepresentation="title"
+      />
+
+      <Resource
+        name="challenges"
+        list={ChallengeList}
+        create={ChallengeCreate}
+        edit={ChallengeEdit}
+        recordRepresentation="question"
+      />
+
+      <Resource
+        name="challengeOptions"
+        list={ChallengeOptionList}
+        create={ChallengeOptionCreate}
+        edit={ChallengeOptionEdit}
+        recordRepresentation="text"
+        options={{ label: "Challenge Options" }}
       />
     </Admin>
   );
